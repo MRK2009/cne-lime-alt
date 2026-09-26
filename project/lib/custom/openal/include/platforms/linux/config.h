e37f764e77a30b15de2b65aed86857a81fa718af
Rafi <mohamedrafikhan2009@gmail.com>
2026-09-26 16:13:30 +0100

    Update config.h

--- project/lib/custom/openal/include/platforms/linux/config.h
+++ project/lib/custom/openal/include/platforms/linux/config.h
@@ -14,7 +14,7 @@
 /* #define HAVE_PTHREAD_NP_H */
 
 /* Define if we have cpuid.h */
-#if defined(__i386__) || defined(__x86_64__)
+#if !defined(RASPBERRYPI) && (defined(__i386__) || defined(__x86_64__))
 #define HAVE_CPUID_H
 #endif
 
@@ -25,7 +25,7 @@
 /* #define HAVE_GUIDDEF_H */
 
 /* Define if we have GCC's __get_cpuid() */
-#if defined(__i386__) || defined(__x86_64__)
+#if !defined(RASPBERRYPI) && (defined(__i386__) || defined(__x86_64__))
 #define HAVE_GCC_GET_CPUID
 #endif
 
